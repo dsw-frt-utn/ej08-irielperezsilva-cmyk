8a2f524d8482cf2e4698becb1cdf11bccfc13cfd
Iriel <irielperezsilva@gmail.com>
2026-05-04 01:29:55 -0300

    Problema 2 resuelto

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine(ProductHelper.ObtenerEtiquetaProducto (1, "Producto", 100));
+
+            var p2 = new Problema2();
+            Console.WriteLine(p2.CrearResumenVenta(1, "Lapiz", 2, 50)); 
         }
     }
 }
