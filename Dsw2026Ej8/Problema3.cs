5f1e3d5a9f32e6449d363e0d13e502b7d0da7d92
Iriel <irielperezsilva@gmail.com>
2026-05-04 01:46:19 -0300

    Problema 3 resuelto

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int copiaValor = originalValue;
+            copiaValor++;
+
+            Product copiaProducto = product;
+            copiaProducto.CambiarDescripcion("Modificar");
+
+            return $"{originalValue}-{copiaValor}-{product.Description}";
+
+        }
+
+    }
+
+
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -7,7 +7,13 @@
             Console.WriteLine(ProductHelper.ObtenerEtiquetaProducto (1, "Producto", 100));
 
             var p2 = new Problema2();
-            Console.WriteLine(p2.CrearResumenVenta(1, "Lapiz", 2, 50)); 
+            Console.WriteLine(p2.CrearResumenVenta(1, "Lapiz", 2, 50));
+
+            var prod = new Product("Original");
+            var p3 = new Problema3();
+
+            Console.WriteLine(p3.CompararCopias(5, prod)); 
+
         }
     }
 }
